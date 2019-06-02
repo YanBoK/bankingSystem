9fed7d8772e70e61694b8105d0fdf3bc858bfa31
YanBoK <1687100393@qq.com>
2019-06-02 17:28:56 +0800

    项目框架

---
+++ yinDeng-common/src/main/java/com/springcloud/common/client/GoodsServiceClient.java
@@ -0,0 +1,59 @@
+package com.springcloud.common.client;
+
+import java.util.Map;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.extlight.springcloud.common.model.Order;
+import com.extlight.springcloud.common.vo.Result;
+
+@FeignClient(value="GOODS")
+public interface GoodsServiceClient {
+
+	/**
+	 * get方式路径传值
+	 * @param goodsId
+	 * @return
+	 */
+	@RequestMapping(value="/goods/goodsInfo/{goodsId}" , method = RequestMethod.GET)
+	public Result goodsInfo(@PathVariable("goodsId") String goodsId);
+	
+	/**
+	 * 测试传Map,都是String
+	 * @param map
+	 * @return
+	 */
+	@RequestMapping(value="/goods/goodInfo",method = RequestMethod.POST)
+	public Result goodInfo(@RequestParam Map<String, Object> map);
+	
+	/**
+	 * 测试传多个参数
+	 * @param goodsId
+	 * @param oderId
+	 * @return
+	 */
+	@RequestMapping(value="/goods/goods",method = RequestMethod.POST)
+	public Result goods(@RequestParam("goodsId") String goodsId,@RequestParam("orderId") String oderId);
+	
+	/**
+	 * 测试传Map,带自定义对象
+	 * @param map
+	 * @return
+	 */
+	@RequestMapping(value="/goods/goodMapObjectInfo",method = RequestMethod.POST)
+	public Result goodMapObjectInfo(@RequestParam Map<String,Object> map);
+	
+	/**
+	 * 测试传Object
+	 * @param order
+	 * @return
+	 */
+	@RequestMapping(value="/goods/goodForObject",method = RequestMethod.POST)
+	public Result goodForObject(@RequestBody Order order);
+	
+}
