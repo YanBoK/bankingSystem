adc735d4cd02277e2c2da619e56c783b31b365fa
82455 <82455@ChenHao>
2019-06-03 18:51:54 +0800

    在在在在在次提交

--- yinDeng-backstage/src/main/java/com/springcloud/backstage/dao/AccountPowerMapper.java
+++ yinDeng-backstage/src/main/java/com/springcloud/backstage/dao/AccountPowerMapper.java
@@ -5,6 +5,6 @@ import java.util.List;
 import com.springcloud.backstage.pojo.AccountPower;
 
 public interface AccountPowerMapper {
-	/**查询全部AccountPower表*/
+	/**查询全部AccountPower表数据*/
 	List<AccountPower> queryAll();
 }
