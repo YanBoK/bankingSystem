3b04753cc49d49dee43f0a75976cf342ac825a19
YanBoK <1687100393@qq.com>
2019-06-04 11:07:33 +0800

    1231

---
+++ yinDeng-users/src/main/java/com/springcloud/users/dao/AccountPowerMapper.java
@@ -0,0 +1,11 @@
+package com.springcloud.users.dao;
+
+import java.util.List;
+
+import com.springcloud.users.pojo.Accountpower;
+
+public interface AccountPowerMapper {
+	
+	List<Accountpower>  queryAll();
+	
+}
