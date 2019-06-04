7f781683cb30005d0f55c34bb6e2ccb3cb8bab08
Administrator <Administrator@LX>
2019-06-04 11:43:27 +0800

    添加User实体类

---
+++ yinDeng-backstage/src/main/java/com/springcloud/backstage/pojo/User.java
@@ -0,0 +1,14 @@
+package com.springcloud.backstage.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class User {
+	private Integer SSS;
+	private String userName;
+
+}
